34902447b2ab10a4cc9974a987a5d279819d5006
medali <medali.rekik.ing@gmail.com>
2023-07-14 18:00:29 +0200

    first commit

---
+++ src/test/java/com/locationvoiture/locationvoiture/LocationVoitureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locationvoiture.locationvoiture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationVoitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
